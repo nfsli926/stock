883f9c626aaf590efb52965629b9df4332a75d16
nfsli926 <litao926@live.com>
2015-09-28 13:42:15 +0800

    新增加对于股票前复权数据的写入功能

--- python/com/nfs/db.py
+++ python/com/nfs/db.py
@@ -1,8 +1,20 @@
 # coding=utf-8
+import util.dbutil as dbutil
+import re
+import sys
+import csv
+import time
 import MySQLdb
+from sqlalchemy import create_engine
+import tushare as ts
 
 #数据库查询功能
-conn = MySQLdb.connect(host='localhost',user='root',passwd='123456',db='stock')
-cursor = conn.cursor()
-n = cursor.execute("select * from cj")
-print n
+#conn = MySQLdb.connect(host='localhost',user='root',passwd='123456',db='stock')
+#cursor = conn.cursor()
+#n = cursor.execute("select * from cj")
+
+stockDf =ts.get_stock_basics()
+sssj = str(stockDf.ix['000001']['timeToMarket']) #上市日期YYYYMMDD
+print sssj[0:4]
+
+#print n
--- python/com/nfs/importdb.py
+++ python/com/nfs/importdb.py
@@ -4,17 +4,34 @@ import util.dbutil as dbutil
 import re
 import sys
 import csv
+import time
 import MySQLdb
+from sqlalchemy import create_engine
+import tushare as ts
 try:
     #首先取出所有股票的代码，然后取得股票的上市时间，根据上市时间按年增加数据，一直进行循环
     conn = MySQLdb.connect(host='localhost',user='root',passwd='123456',db='stock',charset="utf8")
     cursor = conn.cursor()
-    sql ="select * from stock where stockno like\'00%\' or stockno like\'60%\' or stockno like\'3%\'"
+    sql ="select * from stock_basic"
     cursor.execute(sql)
+    stockDf =ts.get_stock_basics()
+
     #通过循环获得股票的基本信息并写入到数据库中，并实现定义好股票的相关基本信息与数据库的字段相对应
     for row in cursor.fetchall():
-        stockno = str(row[1])
+        stockno = str(row[0])
+        print stockno
         #获得上市时间
-        dbutil.get_qfq_date(stockno)
+        sssj = str(stockDf.ix[stockno]['timeToMarket']) #上市日期YYYYMMDD
+        ssYear =int(sssj[0:4])
+        currentYear = int(time.strftime('%Y',time.localtime(time.time())))
+
+        while ssYear<=currentYear:
+            start = str(ssYear)+"-01-01"
+            end = str(ssYear)+"-12-31"
+            dbutil.get_qfq_date(stockno,start,end)
+            print stockno + str(ssYear)+"success"
+            ssYear = ssYear+1
+       #取得当前的年月，并按年进行循环，插入数据相关数据
+
 except Exception,e:
     e.message
