aa89f1ef54971176e9f675bb73d8fcd02d4e0ce4
nfsli926 <litao926@live.com>
2016-04-15 15:29:55 +0800

    增加获取下个月的方法

--- python/com/nfs/util/DateUtil.py
+++ python/com/nfs/util/DateUtil.py
@@ -3,6 +3,7 @@
 __author__ = 'litao'
 import time
 import datetime
+import calendar as cal
 #此处主要编写关于时间的相关函数并作整理
 def get_next_day(mydate):
     time1 = datetime.datetime.strptime(mydate,'%Y-%m-%d')+datetime.timedelta(hours=24)
@@ -18,7 +19,19 @@ def convertDate(sssj):
     timeArray = time.strptime(sssj, "%Y%m%d")
     otherStyleTime = time.strftime("%Y-%m-%d", timeArray)
     return otherStyleTime
+def get_next_month(currentMonth):
+    print int(currentMonth[5:])
+    d = cal.monthrange(int(currentMonth[0:4]),int(currentMonth[5:]))
+    currentMonth_last_day = datetime.date(day=d[1],month=int(currentMonth[5:]), year=int(currentMonth[0:4]))
+    next_Month = currentMonth_last_day + datetime.timedelta(days=1)
+    next_Month = time.strftime('%Y-%m',time.localtime(time.mktime(next_Month.timetuple())))
+    return  next_Month
 
+if __name__ == "__main__":
+    if get_next_month('2016-01'):
+        print "1111"
+    else:
+        print "2222！"
 
 
 
